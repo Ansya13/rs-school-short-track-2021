97a6ae823d35c5d82780e6edf5ff7ebb76ee78af
Anna_L <Lavrinchuk@yahoo.comgit config --listgit config --global user.email Lavrinchuk@yahoo.com>
2021-04-07 22:19:59 +0300

    feat: add solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let counter = 1;
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      counter++;
+    } else if (counter > 1) {
+      result += `${counter}${str[i]}`;
+      counter = 1;
+    } else if (counter === 1) {
+      result += str[i];
+      counter = 1;
+    } else {
+      result = '';
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
