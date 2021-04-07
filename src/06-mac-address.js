560f3ef0cebf87204777305205c8c10bfbd83ef3
Anna_L <Lavrinchuk@yahoo.comgit config --listgit config --global user.email Lavrinchuk@yahoo.com>
2021-04-07 15:54:00 +0300

    feat:add solution

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,12 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const reg = /^[A-Fa-f0-9]{2}(-[A-Fa-f0-9]{2}){5}$/g;
+  if (reg.test(n) === true) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = isMAC48Address;
